659b716bf8f459f96661c881bbe989dee5a408be
chengdai <chengdai.dev@gmail.com>
2024-12-20 00:10:32 +0100

    update reminder info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,9 @@ export default function Home() {
 
   return (
     <div>
-      <p className="text-center italic text-sm mb-4">We deserve better food.</p>
+      <p className="text-center italic text-sm mb-4">
+        restaurants inspection results within Stockholm Stad
+      </p>
       <Search />
       <div className="md:w-1/2 aspect-square md:aspect-[1.2/1] relative mx-auto">
         <Map />

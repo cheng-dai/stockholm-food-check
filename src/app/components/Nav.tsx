ebc13bb437fdda6ab8fe3faab05fd844c331f82f
chengdai <chengdai.dev@gmail.com>
2025-03-24 23:03:41 +0100

    Add smilefaceOutline icon

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -2,13 +2,16 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { SmileFaceOutline } from "./icons";
 
 export default function Nav() {
   const pathname = usePathname();
   return (
     <div className="flex justify-between md:gap-10">
       <Link href="/">
-        <h1 className="text-2xl text-green-600">Safe Savor</h1>
+        <h1 className="text-2xl text-green-600 flex items-center gap-2">
+          Safe Savor <SmileFaceOutline />
+        </h1>
       </Link>
       <Link href={pathname === "/about" ? "/" : "/about"}>
         <h1 className="font-bold text-center text-white md:text-lg ">
